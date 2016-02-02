3656d8f480b9ccf2d17aef7c29ed71b984cbb714
Michael-Nicoletti <michael.nicoletti@RM115-2Win-14.students.stl.yec>
2016-02-02 17:14:20 -0500

    Chris_CMT_SharkAIInitialCommit
    Started work on the shark AI

---
+++ Project Peter's Band UE4/Source/FirstPersonInput/SharkBot.h
@@ -0,0 +1,24 @@
+UCLASS()
+class ASharkBot : public AFirstPersonInput
+{
+	GENERATED_UCLASS_BODY()
+
+
+	UPROPERTY(EditDefaultsOnly, Category = Behaviour)
+	float AttackRange;
+	UPROPERTY(EditDefaultsOnly, Category = Behaviour)
+	float AttackDamage;
+	
+	UPROPERTY(EditAnywhere, Category = Behavior)
+	class UBehaviorTree* BotBehavior;
+
+	UFUNCTION(BlueprintCallable, Category = Behavior)
+	void PerformMeleeAttack();
+
+	float AccumulatedFiretime;
+
+	virtual bool IsFirstPerson() const OVERRIDE;
+
+	virtual void FaceRotation(FRotator NewRotation, float DeltaTime = 0.f) OVERRIDE;
+	bool Attacking;
+};
